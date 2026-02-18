844528b283f57df98ea4fc6e11f4fa0df885eb2e
abhi143x <iabhirajverma35@gmail.com>
2026-02-18 13:19:42 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Footer from './components/layouts/Footer'
+import About from './components/pages/About'
 
 const App = () => {
   return (
     <div>
-      
+      <About />
+      <Footer />
     </div>
   )
 }
