04cb750ae2289080c770e3155988f257535f4041
aloks <aloksingh7587@gmail.com>
2026-02-19 15:38:27 +0530

    new initializr

---
+++ Uvnpl/src/test/java/Univeral_Booking/Uvnpl/UvnplSdApplicationTests.java
@@ -0,0 +1,13 @@
+package Univeral_Booking.Uvnpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UvnplSdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
