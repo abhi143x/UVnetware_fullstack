0e5881a8bd300fee6a7708dcbf786ab89bb15ab7
ASHWINI CHOURASIYA <161760043+ashwini7654@users.noreply.github.com>
2026-02-20 00:22:33 +0530

    My local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
-import React from 'react'
-<<<<<<< HEAD
-import Home from './components/Home.jsx'
-import Navbar from './components/Navbar.jsx'
-=======
-import NavBar from './components/layouts/navBar'
->>>>>>> 9f270d321c17572b8c85649838ec66a892390160
+import React from "react";
+import Home from "./components/Home.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
-  
     <div>
-<<<<<<< HEAD
-      <Navbar />  
+      <Navbar />
       <Home />
-      
-
-=======
-      <NavBar />
->>>>>>> 9f270d321c17572b8c85649838ec66a892390160
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
